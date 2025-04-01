68de7e321c71633fd86b639d312d59e7d72bc8a1
Daniel <dfpires@gmail.com>
2025-04-01 08:00:08 -0300

    Lista 3

---
+++ lista3/js/scripts.js
@@ -0,0 +1,23 @@
+function exe1(){
+    // recupera os dados do usuário
+    let a = Number(document.getElementById("a").value)
+    let b = Number(document.getElementById("b").value)
+    let c = Number(document.getElementById("c").value)
+    let d = Number(document.getElementById("d").value)
+
+    let i = 1 // variável que conta quantas vezes o laço vai se repetir
+    let aux // será utilizada nas trocas das variáveis a, b, c e d
+    while (i <= 3){
+        if (a > b){
+            aux = a; a = b; b = aux;
+        }    
+        if (b > c){
+            aux = b; b = c; c = aux;
+        }
+        if (c > d){
+            aux = c; c = d; d = aux;
+        }
+        i++ // incrementa i
+    }
+    document.getElementById("ordena").innerHTML = `Ordem crescente ${a} ${b} ${c} ${d} e ordem decrescente ${d} ${c} ${b} ${a}`
+}
