deaf4baddf00b15913e6e4277e5ddddb7acc937d
Daniel <dfpires@gmail.com>
2025-05-27 09:03:00 -0300

    dividir para conquistar

--- lista6/exe6.js
+++ lista6/exe6.js
@@ -1,6 +1,6 @@
-function exe6(){
-    let vetor = []
-    // entrada de dados
+function entrada(vet){
+     //  vet recebe vetor (vet = vetor)
+     // entrada de dados
     for(let i=0;i<3;i++){
         // cria o objeto
         let obj = {
@@ -11,32 +11,45 @@ function exe6(){
         // calcula a comissão -> vamos utilizar o . para acessar o campo/atributo do objeto
         obj.comissao = (obj.vendas * obj.percentual) / 100
         // adiciona o objeto no vetor
-        vetor.push(obj)
+        vet.push(obj)
     } // fecha o for
-
+}
+function gerarRelatorio(vet){
     // relação dos vendedores e os valores a receber
     let relatorio = ''
     for(let i=0;i<3;i++){
-        relatorio += `\nVendedor: ${vetor[i].nome} vai receber ${vetor[i].comissao}`
+        relatorio += `\nVendedor: ${vet[i].nome} vai receber ${vet[i].comissao}`
     }
     alert(relatorio)
-    // total das vendas de todos os vendedores
+}
+function totalVendas(vet){
+     // total das vendas de todos os vendedores
     let somaVendas = 0
     for(let i=0;i<3;i++){
-        somaVendas = somaVendas + vetor[i].vendas
+        somaVendas = somaVendas + vet[i].vendas
     }
     alert(`Total de vendas ${somaVendas.toFixed(2)}`)
+}
+function menorMaiorComissao(vet){
     // calcula o maior e menor valor de comissão
-    let objMaior = vetor[0] // primeiro funcionário é o maior
-    let objMenor = vetor[0] // primeiro funcionário é o menor
+    let objMaior = vet[0] // primeiro funcionário é o maior
+    let objMenor = vet[0] // primeiro funcionário é o menor
     for(let i=1;i<3;i++){
-        if (vetor[i].comissao > objMaior.comissao){
-            objMaior = vetor[i]
+        if (vet[i].comissao > objMaior.comissao){
+            objMaior = vet[i]
         }
-        if (vetor[i].comissao < objMenor.comissao){
-            objMenor = vetor[i]
+        if (vet[i].comissao < objMenor.comissao){
+            objMenor = vet[i]
         }
     }
     alert(`${objMaior.nome} vai receber maior comissão de ${objMaior.comissao}`)
     alert(`${objMenor.nome} vai receber menor comissão de ${objMenor.comissao}`)
 }
+function exe6(){
+    let vetor = []
+    entrada(vetor)
+    gerarRelatorio(vetor)
+    totalVendas(vetor)
+    menorMaiorComissao(vetor)
+}
+exe6()
